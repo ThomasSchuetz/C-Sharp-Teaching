46a92b02a319fa35895f015f312071ef07198243
Thomas Schuetz <thomas.schuetz@eon.com>
2020-05-02 20:09:59 +0200

    Extended regular expression exercise inspired by 'https://dotnetfiddle.net/o3Q1tc'

---
+++ CSharpTeaching/RegularExpressions.Tests/PatternsTest.cs
@@ -0,0 +1,141 @@
+﻿// This exercise is strongly inspired by https://dotnetfiddle.net/o3Q1tc
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text.RegularExpressions;
+using System;
+using System.Linq;
+
+namespace RegularExpressions.Tests
+{
+    [TestClass]
+    public class PatternsTest
+    {
+        private string text;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            text = "karunya123.edu  , www.karunya.edu, www.karunya.edu,  " +
+                "http://karunya.edu, https://karunya.edu, www.karunyauniversity.in  ,  " +
+                "https://mykarunya.edu, https://www.karunya.edu  ,  " +
+                "google.com,  google.co.in, www.google.com,  https://www.gmail.com, gmail.com";
+        }
+
+        [TestCleanup]
+        public void CleanUp() => text = null;
+
+        private string[] EvaluatePattern(string pattern)
+        {
+            var expression = new Regex(pattern);
+            var matches = expression.Matches(text);
+
+            Console.WriteLine("Found matches: \n");
+            foreach (var match in matches)
+            {
+                Console.WriteLine(match);
+            }
+
+            return (from match in matches select match.Value).ToArray();
+        }
+
+        private string ReplacePatternInText(string pattern, string replacement)
+        {
+            var expression = new Regex(pattern);
+            var replacedText = expression.Replace(text, replacement);
+
+            Console.WriteLine("Replaced text: \n");
+            Console.WriteLine(replacedText);
+            return replacedText;
+        }
+
+        private bool EvaluateResult(string[] expected, string[] actual)
+        {
+            Assert.AreEqual(expected.Length, actual.Length);
+            foreach (var item in expected)
+                Assert.IsTrue(actual.Contains(item));
+
+            return true;
+        }
+
+        [TestMethod]
+        public void Test_01_extract_all_URLs()
+        {
+            // 1. Extract all the URLs
+            string pattern = ""; // Insert pattern to extract the expected result below
+
+            var matches = EvaluatePattern(pattern);
+
+            var expectedResult = new string[] { "karunya123.edu", "www.karunya.edu", "www.karunya.edu", "http://karunya.edu",
+                "https://karunya.edu", "www.karunyauniversity.in", "https://mykarunya.edu",
+                "https://www.karunya.edu", "google.com", "google.co.in", "www.google.com",
+                "https://www.gmail.com", "gmail.com" };
+            Assert.IsTrue(EvaluateResult(expectedResult, matches));
+        }
+
+        [TestMethod]
+        public void Test_02_extract_all_URLs_starting_with_https()
+        {
+            // 2. Display all the URLs which start with https://
+            string pattern = ""; // Insert pattern to extract the expected result below
+
+            var matches = EvaluatePattern(pattern);
+
+            var expectedResult = new string[] { "https://karunya.edu", "https://mykarunya.edu",
+                "https://www.karunya.edu", "https://www.gmail.com" };
+            Assert.IsTrue(EvaluateResult(expectedResult, matches));
+        }
+
+        [TestMethod]
+        public void Test_03_extract_all_URLs_ending_with_edu()
+        {
+            // 3. URLs ending with .edu
+            string pattern = ""; // Insert pattern to extract the expected result below
+
+            var matches = EvaluatePattern(pattern);
+
+            var expectedResult = new string[] { "karunya123.edu", "www.karunya.edu", "www.karunya.edu", "http://karunya.edu",
+                "https://karunya.edu", "https://mykarunya.edu", "https://www.karunya.edu" };
+            Assert.IsTrue(EvaluateResult(expectedResult, matches));
+        }
+
+        [TestMethod]
+        public void Test_04_replace_all_vowels_in_the_text_with_X()
+        {
+            // 4. Replace all the vowels in the text with given 'X'
+            string pattern = ""; // Insert pattern to extract the expected result below
+
+            string result = ReplacePatternInText(pattern, "X");
+
+            var expectedResult = "kXrXnyX123.XdX  , www.kXrXnyX.XdX, www.kXrXnyX.XdX,  http://kXrXnyX.XdX, " +
+                "https://kXrXnyX.XdX, www.kXrXnyXXnXvXrsXty.Xn  ,  https://mykXrXnyX.XdX, https://www.kXrXnyX.XdX  ,  " +
+                "gXXglX.cXm,  gXXglX.cX.Xn, www.gXXglX.cXm,  https://www.gmXXl.cXm, gmXXl.cXm";
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void Test_05_replace_all_numbers_in_the_text_with_1()
+        {
+            // 5. Replace all the numbers in the text with 1
+            string pattern = ""; // Insert pattern to extract the expected result below
+
+            string result = ReplacePatternInText(pattern, "1");
+
+            var expectedResult = "karunya111.edu  , www.karunya.edu, www.karunya.edu,  http://karunya.edu, " +
+                "https://karunya.edu, www.karunyauniversity.in  ,  https://mykarunya.edu, https://www.karunya.edu  ,  " +
+                "google.com,  google.co.in, www.google.com,  https://www.gmail.com, gmail.com";
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void Test_06_display_all_duplicate_urls()
+        {
+            // 6. Display all duplicate URLs
+            var expectedResult = new string[] { "www.karunya.edu", "www.karunya.edu" };
+
+            // Your code belongs here!
+            // Also adjust the assert statement below
+
+            Assert.AreEqual(expectedResult, null);
+        }
+    }
+}
