41d939c5cab62ff39fdc317296253ae9ccca515e
Thomas Schuetz <thomas.schuetz@eon.com>
2020-06-28 16:23:06 +0200

    Provide solutions with query syntax and method syntax

--- CSharpTeachingSolutions/RefactoringWithLINQ/Library.cs
+++ CSharpTeachingSolutions/RefactoringWithLINQ/Library.cs
@@ -6,19 +6,25 @@ namespace RefactoringWithLINQ
     public class Library
     {
         private readonly List<Book> books = new List<Book>();
-        
+
         public void AddBook(Book book) => books.Add(book);
 
-        public List<Book> BooksOfAuthor(string author) => FilterBooks(book => book.Author == author);
+        public List<Book> BooksOfAuthor(string author)
+        {
+            return (from book in this.books where book.Author == author select book).ToList();
+            return this.books.Where(book => book.Author == author).ToList();
+        }
 
-        public List<Book> BooksWrittenAfter(int year) => FilterBooks(book => book.PublicationYear > year);
+        public List<Book> BooksWrittenAfter(int year)
+        {
+            return (from book in this.books where book.PublicationYear > year select book).ToList();
+            return this.books.Where(book => book.PublicationYear > year).ToList();
+        }
 
-        private List<Book> FilterBooks(System.Func<Book, bool> filter) => books.Where(filter).ToList();
-
-        // Alternative solution for filtering:
-        //public List<Book> BooksOfAuthor(string author) => books.Where(book => book.Author == author).ToList();
-        //public List<Book> BooksWrittenAfter(int year) => books.Where(book => book.PublicationYear > year).ToList();
-
-        public List<Book> SortBooksByYear() => books.OrderBy(book => book.PublicationYear).ToList();
+        public List<Book> SortBooksByYear()
+        {
+            return (from book in this.books orderby book.PublicationYear select book).ToList();
+            return this.books.OrderBy(b => b.PublicationYear).ToList();
+        }
     }
 }
